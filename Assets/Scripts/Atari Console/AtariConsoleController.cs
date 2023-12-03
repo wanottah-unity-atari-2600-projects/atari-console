35969bfab05806a33a5fd96d1b7075d82e161893
Wanottah <wanottah@gmail.com>
2023-12-03 12:39:06 +0000

    Original source code

---
+++ Assets/Scripts/Atari Console/AtariConsoleController.cs
@@ -0,0 +1,103 @@
+﻿
+using UnityEngine;
+
+//
+// Atari Video Game Console Controller v2019.12.17
+//
+// v2021.12.10
+//
+
+public class AtariConsoleController : MonoBehaviour
+{
+    public static AtariConsoleController atariConsoleController;
+
+
+
+    private void Awake()
+    {
+        atariConsoleController = this;
+    }
+
+
+    public void CoinSlotControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.CoinSlotControl();
+    }
+
+
+    // select options control
+    public void OptionsControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.OptionsControl();
+    }
+
+
+    // set options control
+    public void SetOptionsControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.SetOptionsControl();
+    }
+
+
+    // select tv type
+    public void TvTypeControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.TvTypeControl();
+    }
+
+
+    public void LeftDifficultyControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.LeftDifficultyControl();
+    }
+
+
+    public void RightDifficultyControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.RightDifficultyControl();
+    }
+
+
+    public void MasterVolumeControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.MasterVolumeControl();
+    }
+
+
+    public void MusicVolumeControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.MusicVolumeControl();
+    }
+
+
+    public void SfxVolumeControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.SfxVolumeControl();
+    }
+
+
+    public void GameNumberSelectControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.GameNumberControl();
+    }
+
+
+    public void GameResetControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.GameResetControl();
+    }
+
+
+    public void GameResumeControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.GameResumeControl();
+    }
+
+
+    public void GameStartControl()
+    {
+        AtariConsoleCPU.atariConsoleCPU.GameStartControl();
+    }
+
+
+} // end of class
---
+++ Assets/Scripts/Atari Console/Audio/MusicVolumeController.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+//
+// Atari Video Game Console Controller v2020.12.19
+//
+// Audio Music Volume Controller
+//
+// created 2019.12.17
+//
+
+public class MusicVolumeController : MonoBehaviour
+{
+    private Text musicVolumeControlText;
+
+
+    private void Start()
+    {
+        musicVolumeControlText = GetComponent<Text>();
+
+        GetComponentInParent<Slider>().onValueChanged.AddListener(MusicVolumeControl);
+    }
+
+
+    public void MusicVolumeControl(float musicVolume)
+    {
+        AudioController.audioController.SetMusicVolume(musicVolume);
+
+        musicVolumeControlText.text = (musicVolume * 10).ToString("0");
+    }
+
+
+} // end of class
